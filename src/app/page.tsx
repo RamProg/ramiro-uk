ab56d01a6a1f10614a17e220ca534097444af371
RamProg <ramirodlp@gmail.com>
2023-06-04 20:53:31 +0100

    add new profile pic and responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,10 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <main className="flex justify-center items-center h-screen w-screen bg-gray-950">
-      <div className="flex flex-row items-stretch">
+      <div className="flex flex-col sm:flex-row items-stretch">
         <Image
-          className="rounded-full h-fit shadow-[15px_15px_60px_-15px_rgba(255,255,255,0.5)]"
-          src="/me.jpg"
+          className="rounded-full h-fit shadow-[15px_15px_60px_-15px_rgba(255,87,51,0.5)]"
+          src="/mebg.png"
           width={300}
           height={300}
           alt="Me"
