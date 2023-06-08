de725b8b9dd342ba87f1d6804ccc8c705af62c17
RamProg <ramirodlp@gmail.com>
2023-06-08 18:57:33 +0100

    change to transparent image with gradient; add fonts

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,15 +1,17 @@
 import Image from 'next/image';
 import Description from './Description/Description';
+import { isMobile } from 'react-device-detect';
 
 const Main = () => {
+  const imageSize = isMobile ? 0 : 320;
   return (
     <main className="flex justify-center sm:items-center h-screen bg-gray-950">
-      <div className="flex flex-col sm:flex-row items-center sm:items-stretch mt-10 sm:mt-0">
+      <div className="flex flex-col sm:flex-row items-center sm:items-stretch mt-10 sm:mt-0 h-64 sm:h-80">
         <Image
-          className="rounded-full h-fit shadow-[15px_15px_60px_-15px_rgba(255,87,51,0.5)] bg-orange-400 sm:w-1/2"
-          src="/mebg.png"
-          width={300}
-          height={300}
+          className="rounded-full shadow-[15px_15px_60px_-15px_rgba(255,87,51,0.5)] bg-gradient-to-br from-pink-500 from-1% via-orange-400 via-40% to-yellow-300 to-90% sm:w-1/2 border-8 border-red-500"
+          src="/nobg1.png"
+          width={256}
+          height={256}
           alt="Me"
         />
         <div className="w-5 h-5" />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
