67943fe8e699d5b69139da4ecc6347624a39319e
RamProg <ramirodlp@gmail.com>
2023-06-09 16:06:25 +0100

    fix path

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -16,7 +16,7 @@ const Main = () => {
       <div className="flex flex-col sm:flex-row items-center sm:items-stretch mt-10 sm:mt-0 h-64 sm:h-80 sm:w-[40rem]">
         <Image
           className="rounded-full shadow-[15px_15px_60px_-15px_rgba(255,87,51,0.5)] bg-gradient-to-br from-pink-500 from-1% via-orange-400 via-40% to-yellow-300 to-90% sm:w-1/2 border-8 border-red-500"
-          src="/me-nobg.png"
+          src="/images/me-nobg.png"
           width={imageSize}
           height={imageSize}
           alt="Me"
