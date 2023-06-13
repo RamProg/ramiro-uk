baca656cd6bf3a34a445ccc687cbf46283a7f8a0
RamProg <ramirodlp@gmail.com>
2023-06-13 14:51:48 +0100

    adjust mobile spacion of nav screen

--- src/components/Main/Landing/Nav.tsx
+++ src/components/Main/Landing/Nav.tsx
@@ -32,7 +32,7 @@ const Nav = () => {
     <nav
       id='nav'
       className='flex justify-center p-10 bg-orange-700 h-dynamicScreen sm:absolute sm:top-0 sm:left-0 sm:h-12 sm:p-0 sm:w-screen sm:bg-transparent sm:items-center'>
-      <ul className='flex flex-col items-center content-around justify-between sm:flex-row sm:w-[48rem]'>
+      <ul className='flex flex-col items-center content-around justify-around sm:justify-between sm:flex-row sm:w-[48rem]'>
         {links.map((link, i) => {
           return (
             <Link
