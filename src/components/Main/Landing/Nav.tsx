bfd004582bb778b5107ca90a1b3a41e380467de3
RamProg <ramirodlp@gmail.com>
2023-06-13 14:49:40 +0100

    change navbar width and spacing

--- src/components/Main/Landing/Nav.tsx
+++ src/components/Main/Landing/Nav.tsx
@@ -32,7 +32,7 @@ const Nav = () => {
     <nav
       id='nav'
       className='flex justify-center p-10 bg-orange-700 h-dynamicScreen sm:absolute sm:top-0 sm:left-0 sm:h-12 sm:p-0 sm:w-screen sm:bg-transparent sm:items-center'>
-      <ul className='flex flex-col items-center justify-evenly sm:flex-row sm:justify-between'>
+      <ul className='flex flex-col items-center content-around justify-between sm:flex-row sm:w-[48rem]'>
         {links.map((link, i) => {
           return (
             <Link
@@ -41,7 +41,7 @@ const Nav = () => {
               className='text-white'
               key={link.text}>
               <li
-                className={`flex items-center justify-center w-48 p-2 border-2 rounded-md h-14 sm:h-8 sm:w-auto sm:border-0 sm:text-yellow-300 sm:ml-8 sm:mr-8 sm:mt-8 sm:hover:text-yellow-100 ${cedarville.variable} sm:font-serif sm:text-2xl`}>
+                className={`flex items-center justify-center w-48 p-2 border-2 rounded-md h-14 sm:h-8 sm:w-auto sm:border-0 sm:text-yellow-300 sm:mt-8 sm:hover:text-yellow-100 ${cedarville.variable} sm:font-serif sm:text-2xl`}>
                 {link.text.toLocaleLowerCase()}
               </li>
             </Link>
