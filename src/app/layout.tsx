ddd4e00aafe055eef678e3b1de2fa1c348fa6734
RamProg <ramirodlp@gmail.com>
2023-06-15 17:03:53 +0100

    new favicon and refactor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,29 +2,9 @@ import '@/app/globals.css';
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import { roboto } from '@/assets/fonts';
+import { metadata } from './metadata';
 
 config.autoAddCss = false;
-
-export const metadata = {
-  title: 'Ramiro',
-  description: 'Leading Engineering Teams',
-  themeColor: '#ff5733',
-  openGraph: {
-    title: 'Ramiro',
-    description: 'Leading Engineering Teams',
-    url: 'https://www.ramiro.uk',
-    siteName: 'Ramiro',
-    images: [
-      {
-        url: '/images/opengraph.png',
-        width: 854,
-        height: 425,
-      },
-    ],
-    locale: 'en_US',
-    type: 'website',
-  },
-};
 
 export default function RootLayout({
   children,
@@ -41,3 +21,5 @@ export default function RootLayout({
     </html>
   );
 }
+
+export { metadata };
---
+++ src/app/metadata.ts
@@ -0,0 +1,55 @@
+export const metadata = {
+  title: 'Ramiro',
+  description: 'Leading Engineering Teams',
+  themeColor: '#ff5733',
+  openGraph: {
+    title: 'Ramiro',
+    description: 'Leading Engineering Teams',
+    url: 'https://www.ramiro.uk',
+    siteName: 'Ramiro',
+    images: [
+      {
+        url: '/images/opengraph.png',
+        width: 854,
+        height: 425,
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  icons: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      url: '/images/favicon-32x32.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      url: '/images/favicon-16x16.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: 'any',
+      url: '/images/favicon.ico',
+    },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      url: '/images/apple-touch-icon.png',
+    },
+    {
+      rel: 'shortcut icon',
+      sizes: '192x192',
+      url: '/images/android-chrome-192x192.png',
+    },
+    {
+      rel: 'shortcut icon',
+      sizes: '512x512',
+      url: '/images/android-chrome-512x512.png',
+    },
+  ],
+};
