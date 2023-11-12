15b0675b9f3220ede05058c31e171286fe9b1e58
Ramiro <64564058+RamProg@users.noreply.github.com>
2023-11-12 18:56:12 +0000

    Update cv url

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
       {
         source: '/cv',
         destination:
-          'https://drive.google.com/file/d/1jiInnYoGrnj8hX7MGYIS-14mdLeS4989/view',
+          'https://drive.google.com/file/d/1huO-gbsx88v5zIhoA1DmPvQ5OOiAPTUl/view',
         permanent: false,
         basePath: false,
       },
