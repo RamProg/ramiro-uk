5dc9f255f7576d7272875d3b649b84994d57a0ae
RamProg <ramirodlp@gmail.com>
2023-12-20 13:49:26 +0000

    add size to floating scroll; change use scroll calculation

--- src/hooks/useScroll.tsx
+++ src/hooks/useScroll.tsx
@@ -1,6 +1,7 @@
 const useScroll = () => {
   const scrollToNext = () => {
-    const top = window.scrollY > 50 ? 0 : window.innerHeight;
+    const isAtBottom = window.scrollY + window.innerHeight >= document.documentElement.scrollHeight;
+    const top = isAtBottom ? 0 : document.documentElement.scrollHeight - window.innerHeight;
 
     setTimeout(function () {
       window.scrollTo({ top, behavior: 'smooth' });
